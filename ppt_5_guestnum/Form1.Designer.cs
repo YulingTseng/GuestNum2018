c3e8f3195debca417691735fb09ee45b832a6a98
Yuling Tseng <enjoy7899@gmail.com>
2020-03-23 01:25:21 +0800

    GuestNum2018

---
+++ ppt_5_guestnum/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace ppt_5_guestnum
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MessageLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.GuessTxtBox = new System.Windows.Forms.TextBox();
+            this.CountLabel = new System.Windows.Forms.Label();
+            this.ComfirmBtn = new System.Windows.Forms.Button();
+            this.ReplayBtn = new System.Windows.Forms.Button();
+            this.ExitBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MessageLabel
+            // 
+            this.MessageLabel.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.MessageLabel.Location = new System.Drawing.Point(63, 68);
+            this.MessageLabel.Name = "MessageLabel";
+            this.MessageLabel.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.MessageLabel.Size = new System.Drawing.Size(280, 56);
+            this.MessageLabel.TabIndex = 0;
+            this.MessageLabel.Text = "1<?<999";
+            this.MessageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "猜數字";
+            // 
+            // GuessTxtBox
+            // 
+            this.GuessTxtBox.Location = new System.Drawing.Point(176, 139);
+            this.GuessTxtBox.Name = "GuessTxtBox";
+            this.GuessTxtBox.Size = new System.Drawing.Size(139, 25);
+            this.GuessTxtBox.TabIndex = 2;
+            // 
+            // CountLabel
+            // 
+            this.CountLabel.AutoSize = true;
+            this.CountLabel.Location = new System.Drawing.Point(67, 212);
+            this.CountLabel.Name = "CountLabel";
+            this.CountLabel.Size = new System.Drawing.Size(67, 15);
+            this.CountLabel.TabIndex = 3;
+            this.CountLabel.Text = "共猜了次";
+            // 
+            // ComfirmBtn
+            // 
+            this.ComfirmBtn.Location = new System.Drawing.Point(38, 292);
+            this.ComfirmBtn.Name = "ComfirmBtn";
+            this.ComfirmBtn.Size = new System.Drawing.Size(98, 30);
+            this.ComfirmBtn.TabIndex = 4;
+            this.ComfirmBtn.Text = "開始";
+            this.ComfirmBtn.UseVisualStyleBackColor = true;
+            this.ComfirmBtn.Click += new System.EventHandler(this.ComfirmBtn_Click);
+            // 
+            // ReplayBtn
+            // 
+            this.ReplayBtn.Location = new System.Drawing.Point(161, 292);
+            this.ReplayBtn.Name = "ReplayBtn";
+            this.ReplayBtn.Size = new System.Drawing.Size(98, 30);
+            this.ReplayBtn.TabIndex = 5;
+            this.ReplayBtn.Text = "暫停";
+            this.ReplayBtn.UseVisualStyleBackColor = true;
+            // 
+            // ExitBtn
+            // 
+            this.ExitBtn.Location = new System.Drawing.Point(284, 292);
+            this.ExitBtn.Name = "ExitBtn";
+            this.ExitBtn.Size = new System.Drawing.Size(98, 30);
+            this.ExitBtn.TabIndex = 6;
+            this.ExitBtn.Text = "結束";
+            this.ExitBtn.UseVisualStyleBackColor = true;
+            this.ExitBtn.Click += new System.EventHandler(this.ExitBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 450);
+            this.Controls.Add(this.ExitBtn);
+            this.Controls.Add(this.ReplayBtn);
+            this.Controls.Add(this.ComfirmBtn);
+            this.Controls.Add(this.CountLabel);
+            this.Controls.Add(this.GuessTxtBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MessageLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label MessageLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox GuessTxtBox;
+        private System.Windows.Forms.Label CountLabel;
+        private System.Windows.Forms.Button ComfirmBtn;
+        private System.Windows.Forms.Button ReplayBtn;
+        private System.Windows.Forms.Button ExitBtn;
+    }
+}
+
---
+++ ppt_5_guestnum/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ppt_5_guestnum
+{
+    public partial class Form1 : Form
+    {
+        int Count, Answer;
+        int Max, Min;
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random rn = new Random();
+            Count = 0;  //設定猜測次數的初值
+            Answer = rn.Next(1, 1000);
+            Min = 1; //設定答案範圍的最小值
+            Max = 999; //設定答案範圍的最大值
+            MessageLabel.Text = Min.ToString() + " < ? < " + Max.ToString();
+            CountLabel.Text = "共猜了" + Count.ToString() + "次";
+            GuessTxtBox.Text =" "; //將輸入方塊裡的文字設為空白
+            GuessTxtBox.Focus(); //讓輸入方塊擁有輸入焦點，即游標會停在此方塊內
+
+        }
+
+        private void ExitBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        
+        private void ComfirmBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int num = int.Parse(GuessTxtBox.Text); //將輸入文字方塊的內容轉成數字
+                Count++;
+                if (num < Answer)
+                {
+                    if (num > Min) Min = num;
+                    MessageBox.Show("再大一點");
+                    MessageLabel.Text = Min.ToString() + " < ? < " + Max.ToString();
+                }
+                else if (num > Answer)
+                {
+                    if (num < Max) Max = num;
+                    MessageBox.Show("再小一點");
+                    MessageLabel.Text = Min.ToString() + " < ? < " + Max.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("賓果");
+                    MessageLabel.Text = "答案是" + Answer.ToString();
+                }
+                CountLabel.Text = "共猜了" + Count.ToString() + "次";
+                GuessTxtBox.Text = "";  //清除文字方塊的內容
+                GuessTxtBox.Focus();    //讓文字方塊重新取得輸入焦點
+            }
+
+            //當有錯誤發生時，不做任何處理，直接清除文字方塊的內容並讓文字方塊重新取得輸入焦點
+            catch
+            {
+                GuessTxtBox.Text = "";
+                GuessTxtBox.Focus();
+            }
+        }
+    }
+}
